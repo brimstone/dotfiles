c948534c5a564a7d44220dfac7a87161580e29a1
Matt <brimstone@the.narro.ws>
2013-09-08 20:24:13 -0400

    Adding some dotfiles for vim and tmux

---
+++ tmux.bash
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+################################################################################
+
+source ~/bin/bashter
+
+line=( )
+prio=( )
+
+################################################################################
+# sysupdates
+if [ "$STAT_SYSUPDATES" -gt 0 ]; then
+	line[${#line[*]}]=$(printf "Updates: %s" $STAT_SYSUPDATES)
+	prio[${#prio[*]}]="high"
+fi
+
+################################################################################
+# interfaces
+gw=$(ip route list 0/0 | awk '{print $5}')
+ips=$(ip -o addr | awk "/inet /{
+	if (\$2==\"$gw\"){printf \"#[fg=colour64]\"};
+	if (\$2!=\"lo\"){printf \$2 \":\" \$4 \" \"}
+	if (\$2==\"$gw\"){printf \"#[fg=colour235, bg=colour254]\"};
+}")
+line[${#line[*]}]=${ips%% }
+prio[${#prio[*]}]="low"
+
+################################################################################
+# battery
+if [ "$STAT_BATT_C" = "0" ]; then
+	if [ "$STAT_BATT_P" -lt "10" ]; then
+		line[${#line[*]}]="#[fg=colour160]D:${STAT_BATT_T}(${STAT_BATT_P}%)#[fg=colour235, bg=colour254]"
+	else
+		line[${#line[*]}]="D:${STAT_BATT_T}(${STAT_BATT_P}%)"
+	fi
+	prio[${#prio[*]}]="high"
+elif [ "$STAT_BATT_C" = "1" ]; then
+	line[${#line[*]}]="C:${STAT_BATT_T}(${STAT_BATT_P}%)"
+	prio[${#prio[*]}]="high"
+fi
+
+################################################################################
+# load
+if [ $(sed -e 's/\.//' <<< "$STAT_LOAD") -gt 100 ]; then
+	line[${#line[*]}]="#[fg=colour160]L:${STAT_LOAD}#[fg=colour235, bg=colour254]"
+	prio[${#prio[*]}]="high"
+else
+	line[${#line[*]}]="L:$STAT_LOAD"
+	prio[${#prio[*]}]="low"
+fi
+
+################################################################################
+# ssh-agent
+if [ "$STAT_SSH_AGENT" = "0" ]; then
+	line[${#line[*]}]="#[fg=colour160]No Keys#[fg=colour235, bg=colour254]"
+	prio[${#prio[*]}]="high"
+fi
+
+################################################################################
+# Memory
+if [ "$STAT_MEM_UP" -gt 89 ]; then
+	line[${#line[*]}]="#[fg=colour160]R:$STAT_MEM_F($STAT_MEM_UP%)$(ps -e -orss,pid,comm | sort -rn | awk 'NR==1 {printf ":" $3 "(" ($1/1024) "M)(" $2 ")" }')#[fg=colour235, bg=colour254]"
+	prio[${#prio[*]}]="high"
+elif [ "$STAT_MEM_UP" -gt 49 ]; then
+	line[${#line[*]}]="R:${STAT_MEM_F}M($STAT_MEM_UP%)$(ps -e -orss,pid,comm | sort -rn | awk 'NR==1 {printf ":" $3 "(" ($1/1024) "M)(" $2 ")" }')"
+	prio[${#prio[*]}]="high"
+else
+	line[${#line[*]}]="R:${STAT_MEM_U}M($STAT_MEM_UP%)"
+	prio[${#prio[*]}]="low"
+fi
+
+################################################################################
+# If we use unison on this system, show the time since the last sync
+if [ -e unison.log ]; then
+	if [ $(ps ax | grep -v grep | grep -c unison) -gt 0 ]; then
+		line[${#line[*]}]="#[fg=colour64]U:Running#[fg=colour235, bg=colour254]"
+	else
+		lastsync=$(date -d "$(grep finish unison.log | tail -n 1 | sed -e "s/^.*changes at //g;s/ on//g")" +%s)
+		now=$(date +%s)
+		diff=$[ $now - $lastsync ]
+		s=$(TZ=UTC date -d@$diff +%H:%M:%S)
+		if [ $diff -gt 120 ]; then
+			line[${#line[*]}]="#[fg=colour160]U:$s#[fg=colour235, bg=colour254]"
+		else
+			line[${#line[*]}]="U:$s"
+		fi
+	fi
+	prio[${#prio[*]}]="high"
+fi
+
+for d in $(seq 1 $STAT_DISK_NUM); do
+	eval "mnt=\"\$STAT_DISK${d}_MNT\""
+	eval "usedp=\"\$STAT_DISK${d}_USEDP\""
+	if [ "$usedp" -gt 80 ]; then
+		eval "free=\$STAT_DISK${d}_FREE"
+		if [ $free -gt 1024 ]; then
+			free=$(( $free / 1024 ))
+			free="${free}GB"
+		else
+			free="${free}MB"
+		fi
+		line[${#line[*]}]="#[fg=colour160]$mnt:$free($usedp%)#[fg=colour235, bg=colour254]"
+		prio[${#prio[*]}]="high"
+	elif [ "$usedp" -gt 50 ]; then
+		eval "used=\$STAT_DISK${d}_USED"
+		if [ $used -gt 1024 ]; then
+			used=$(( $used / 1024 ))
+			used="${used}GB"
+		else
+			used="${used}MB"
+		fi
+		line[${#line[*]}]="$mnt:$used($usedp%)"
+		prio[${#prio[*]}]="low"
+	fi
+done
+
+################################################################################
+# first, gather up everything
+output=""
+for i in $(seq 0 $[${#line[*]} - 1]); do
+	output="$output#[fg=colour254, bg=colour235]\u2b82#[fg=colour235, bg=colour254] ${line[$i]} #[fg=colour235, bg=colour254]\u2b82"
+done
+
+length=$(tmux show-options -g -t main | grep status-right-length)
+## see if it's too long
+if [ $(echo -e "$output $(hostname) XXX MM/DD HH:MM:SS" | sed -E 's/#\[[^]]*\]//g' | wc -c) -gt ${length##* } ]; then
+# if it is, try again with just the "high" priority stuff"
+	output=""
+	for i in $(seq 0 $[${#line[*]} - 1]); do
+		if [ "${prio[$i]}" = "high" ]; then
+			output="$output#[fg=colour254, bg=colour235]\u2b82#[fg=colour235, bg=colour254] ${line[$i]} #[fg=colour235, bg=colour254]\u2b82"
+		fi
+	done
+fi
+echo -e "$output"
---
+++ vimrc
@@ -0,0 +1,97 @@
+" need to further review http://amix.dk/vim/vimrc.html
+map <Esc>j <C-W>j
+map <Esc>k <C-W>k
+map <Esc>h <C-W>h
+map <Esc>l <C-W>l
+
+" Uncomment the following to have Vim jump to the last position when reopening a file
+au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal g'\"" | endif
+
+" supa-awesome syntax highlighting
+syntax on
+filetype on
+set background=dark
+" set cakephp's template extension
+au BufNewFile,BufRead *.ctp set filetype=php
+
+
+" auto close html tags while editing
+" au Filetype html,xml,xsl,php source ~/.vim/scripts/closetag.vim
+
+set foldmethod=indent "zo to open, zc to close
+set foldlevel=99
+
+set number	" turn on line numbers
+set showmatch	" show matching braces and brackets
+set hlsearch	" highlight search results
+set incsearch	" show the first matching result while searching for results
+set history=10	" disable our search history
+
+" set status line
+set statusline=%<%f\ %h%m%r%=%-20.(line=%l,col=%c%V,totlin=%L%)
+set laststatus=2 
+
+set encoding=utf-8
+set modeline
+
+" Intellisense!
+autocmd FileType php set omnifunc=phpcomplete#CompletePHP
+autocmd FileType html set omnifunc=htmlcomplete#CompleteTags
+autocmd FileType javascript set omnifunc=javascriptcomplete#CompleteJS
+autocmd FileType css set omnifunc=csscomplete#CompleteCSS
+autocmd FileType xml set omnifunc=xmlcomplete#CompleteTags
+autocmd FileType c call FileType_c()
+autocmd FileType markdown call FileType_txt()
+autocmd BufNewFile,BufRead *.txt call FileType_txt()
+
+setlocal omnifunc=syntaxcomplete#Complete
+"set cot+=menuone
+
+" for mouse support inside screen
+set ttymouse=xterm2
+"set mouse=a
+
+" use w!! after you open a file to save it with sudo
+cmap w!! w !sudo tee % >/dev/null
+"cmap W w
+"cmap Q q
+
+" Press F4 to toggle highlighting on/off, and show current value.
+:noremap <F4> :set hlsearch! hlsearch?<CR>
+
+map <F5> :!clear; make && make test <RETURN>
+set t_Co=256 
+
+function! FileType_txt()
+	set spell spelllang=en_us
+endfunction
+
+function! FileType_c()
+	set omnifunc=ccomplete#Complete
+endfunction
+
+" Pathogen
+call pathogen#infect()
+
+" Solarize
+let g:solarized_termtrans = 1
+let g:solarized_termcolors=256
+set background=dark
+colorscheme solarized
+
+" Powerline
+let g:Powerline_symbols = 'fancy'
+"let g:Powerline_theme = 'solarized256'
+let g:Powerline_colorscheme = 'solarized256'
+
+" Network Oriented Reading, Writing and Browsing
+let g:netrw_silent	= 1
+
+" Tab options
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set noexpandtab 
+
+" Unite
+nnoremap <C-p> :Unite file<cr>
